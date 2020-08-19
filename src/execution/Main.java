6a8cb6926cbb76e2b00374c4b42e43a2d8b8e0a4
This PC <khushi.agarwal_cs19@gla.ac.in>
2020-08-19 20:29:31 +0530

    add the setter methods for the Video.java; initialize the attributes of a Video object

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -5,6 +5,9 @@ import definitons.Video;
 public class Main {
     public static void main(String[] args) {
         Video video = new Video();
+        video.setVideoName("Baahubali: The Beginning");
+        video.setRating(5);
+        video.setCheckOut(false);
         System.out.println(
                 "Video Name: " + video.getVideoName() + ", " +
                         "Video Rating: " + video.getRating() + ", " +
